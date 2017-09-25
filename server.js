bbc75342cf438c74dc55f37c88b40e83aaf3e3e4
James King <hello@jamesmichaelking.com>
2017-09-25 13:56:52 +0100

    Updates

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.use(express.static('dist'))
+app.use(express.static('dist'));
 
 app.listen(3000, function () {
   console.log('View the app at http://localhost:3000/')
-})
+});
