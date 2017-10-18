a12c0bd1ae97c457630229295173b4600e533505
James King <hello@jamesmichaelking.com>
2017-10-18 09:57:08 +0100

    Show Password JS

--- dist/javascript/app.js
+++ dist/javascript/app.js
@@ -1,3 +1,28 @@
 var NSA = NSA || {};
 
+NSA = {
+  showPassword: function () {
+    var $showPassword = $('.show-password');
 
+    $showPassword.each(function (i) {
+
+      var $that = $(this),
+        $pwdFld = $that.find('input:password').eq(0),
+        $checkLbl = $('<label />').prop({ for: 'show-password-' + i }).html('Show password'),
+        $checkBox = $('<input />').prop({ type: 'checkbox', id: 'show-password-' + i }).on('change', function () {
+          var $that = $(this);
+          $pwdFld.prop('type', function () {
+            return $that.prop('checked') ? 'text' : 'password';
+          });
+        }),
+        $cbWrap = $('<div />').prop('class', 'multiple-choice').append($checkBox, $checkLbl);
+
+      $pwdFld.after($cbWrap);
+
+    });
+  }
+};
+
+if ($('.show-password').length > 0) {
+  NSA.showPassword();
+}
