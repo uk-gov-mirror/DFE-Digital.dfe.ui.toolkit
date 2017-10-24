0a46040845de1f5781d0042370a90d83e1f01858
James King <hello@jamesmichaelking.com>
2017-10-24 17:18:04 +0100

    Updated styles for show password control

--- dist/javascript/app.js
+++ dist/javascript/app.js
@@ -7,15 +7,18 @@ NSA = {
     $showPassword.each(function (i) {
 
       var $that = $(this),
-        $pwdFld = $that.find('input:password').eq(0),
-        $checkLbl = $('<label />').prop({ for: 'show-password-' + i }).html('Show password'),
+        $pwdFld = $that.find('input:password'),
+        $checkLbl = $('<label />').prop({ for: 'show-password-' + i }).html('Show'),
         $checkBox = $('<input />').prop({ type: 'checkbox', id: 'show-password-' + i }).on('change', function () {
           var $that = $(this);
           $pwdFld.prop('type', function () {
             return $that.prop('checked') ? 'text' : 'password';
           });
+          $checkLbl.html(function () {
+            return $that.prop('checked') ? 'Hide' : 'Show';
+          });
         }),
-        $cbWrap = $('<div />').prop('class', 'multiple-choice').append($checkBox, $checkLbl);
+        $cbWrap = $('<div />').prop('class', 'show-password-control').append($checkBox, $checkLbl);
 
       $pwdFld.after($cbWrap);
 
