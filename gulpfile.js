d9ceb071b0205d13d4d015eb44f952bcf872ace6
James King <hello@jamesmichaelking.com>
2017-11-14 09:42:10 +0000

    Updates including JS build

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,61 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var cleanCSS = require('gulp-clean-css');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const cleanCSS = require('gulp-clean-css');
 
-var input = './src/sass/*.scss';
-var output = './dist/css/';
+const input = './src/sass/*.scss';
+const output = './dist/css/';
 
-var sassOptions;
+let sassOptions;
 sassOptions = {
-    errLogToConsole: true,
-    outputStyle: 'compressed',
-    includePaths: [
-        'node_modules/govuk_frontend_toolkit/stylesheets',
-        'node_modules/govuk-elements-sass/public/sass'
-    ]
+  errLogToConsole: true,
+  outputStyle: 'compressed',
+  includePaths: [
+    'node_modules/govuk_frontend_toolkit/stylesheets',
+    'node_modules/govuk-elements-sass/public/sass',
+  ],
 };
 
-gulp.task('copy-minify', function() {
+gulp.task('copy-minify', () => {
   gulp.src([
-      "node_modules/govuk_template_jinja/assets/stylesheets/fonts.css",
-      "node_modules/govuk_template_jinja/assets/stylesheets/govuk-template.css"
+    'node_modules/govuk_template_jinja/assets/stylesheets/fonts.css',
+    'node_modules/govuk_template_jinja/assets/stylesheets/govuk-template.css',
   ])
-      .pipe(cleanCSS({compatibility: 'ie8'}))
-      .pipe(gulp.dest(output + 'govuk/'));
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(gulp.dest(`${output}govuk/`));
 });
 
-gulp.task('copy-js', function () {
+gulp.task('copy-js', () => {
   gulp.src([
-    "node_modules/jquery/dist/jquery.js"
+    'node_modules/jquery/dist/jquery.js',
   ])
-    .pipe(gulp.dest('./dist/javascript/vendors/'))
+    .pipe(gulp.dest('./dist/javascript/vendors/'));
 });
 
-gulp.task('watch', function() {
+// script paths
+let jsFiles = 'src/javascript/*.js',
+  jsDest = 'dist/javascript';
+
+gulp.task('scripts', function() {
+  return gulp.src(jsFiles)
+    .pipe(concat('app.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(jsDest));
+});
+
+gulp.task('watch', () => {
   gulp.watch(input, ['sass'])
-    .on('change', function(event) {
-      console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    .on('change', (event) => {
+      console.log(`File ${event.path} was ${event.type}, running tasks...`);
     });
 });
 
-gulp.task('sass', function () {
-  return gulp
-    .src(input)
-    .pipe(sass(sassOptions))
-    .pipe(gulp.dest(output));
-});
+gulp.task('sass', () => gulp
+  .src(input)
+  .pipe(sass(sassOptions))
+  .pipe(gulp.dest(output)));
 
 
-gulp.task('default', [ 'sass', 'watch' , 'copy-minify', 'copy-js' ]);
+gulp.task('default', ['sass', 'watch', 'copy-minify', 'copy-js']);
