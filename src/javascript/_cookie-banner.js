05d71e393929f91f3c190ca42ba0e771b1241e80
Jordan Paisley <jordan@cypher.dev>
2020-08-10 16:15:08 +0100

    add logic in IIFE to prevent variables being appended to window

--- src/javascript/_cookie-banner.js
+++ src/javascript/_cookie-banner.js
@@ -1,55 +1,57 @@
-var $cookieBanner = $('.global-cookie-message-dfe-sign-in');
-var $cookieAcceptButton = $cookieBanner.find('button.cookie-accept');
+(function (window) {
+  var $ = window.$;
+  var Cookies = window.Cookies;
 
-var COOKIE_NAMES = {
-  PREFERENCES_SET: 'cookies_preferences_set',
-  POLICY: 'cookies_policy'
-};
-
-var DEFAULT_POLICY = {
-  essential: true,
-  settings: false,
-  usage: false,
-  campaigns: false
-};
-
-var GOVUK_COOKIE_OPTIONS = {
-  expires: 365, // days
-};
-
-if (!window.Cookies.get(COOKIE_NAMES.PREFERENCES_SET)) {
-  window.Cookies.set(
-    COOKIE_NAMES.POLICY,
-    DEFAULT_POLICY,
-    GOVUK_COOKIE_OPTIONS
-  );
-
-  $cookieBanner.slideDown(function () {
-
-    $cookieAcceptButton.click(function () {
-      var acceptedPolicy = $.extend(DEFAULT_POLICY, {}); // deep copy
-      
-      window.Cookies.set(
-        COOKIE_NAMES.PREFERENCES_SET,
-        true,
-        GOVUK_COOKIE_OPTIONS
-      );
-
-      $.each(acceptedPolicy, function (key) {
-        acceptedPolicy[key] = true;
+  var $cookieBanner = $('.global-cookie-message-dfe-sign-in');
+  var $cookieAcceptButton = $cookieBanner.find('button.cookie-accept');
+  
+  var COOKIE_NAMES = {
+    PREFERENCES_SET: 'cookies_preferences_set',
+    POLICY: 'cookies_policy'
+  };
+  
+  var DEFAULT_POLICY = {
+    essential: true,
+    settings: false,
+    usage: false,
+    campaigns: false
+  };
+  
+  var GOVUK_COOKIE_OPTIONS = {
+    expires: 365, // days
+  };
+  
+  if (!Cookies.get(COOKIE_NAMES.PREFERENCES_SET)) {
+    Cookies.set(
+      COOKIE_NAMES.POLICY,
+      DEFAULT_POLICY,
+      GOVUK_COOKIE_OPTIONS
+    );
+  
+    $cookieBanner.slideDown(function () {
+      $cookieAcceptButton.click(function () {
+        var acceptedPolicy = $.extend(DEFAULT_POLICY, {}); // deep copy
+        
+        Cookies.set(
+          COOKIE_NAMES.PREFERENCES_SET,
+          true,
+          GOVUK_COOKIE_OPTIONS
+        );
+  
+        $.each(acceptedPolicy, function (key) {
+          acceptedPolicy[key] = true;
+        });
+  
+        Cookies.set(
+          COOKIE_NAMES.POLICY,
+          acceptedPolicy,
+          GOVUK_COOKIE_OPTIONS
+        );
+  
+        $cookieBanner.slideUp();
       });
-
-
-      window.Cookies.set(
-        COOKIE_NAMES.POLICY,
-        acceptedPolicy,
-        GOVUK_COOKIE_OPTIONS
-      );
-
-      $cookieBanner.slideUp();
-      
+    
     });
-  
-  });
-  
-}
+    
+  }
+})(window);
