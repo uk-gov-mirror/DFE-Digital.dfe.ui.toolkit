23741cd5097a854c6f0b788da71065e87b8c627d
Jordan Paisley <jordan@cypher.dev>
2020-08-10 16:07:00 +0100

    add js for sliding cookie banner

--- src/javascript/_cookie-banner.js
+++ src/javascript/_cookie-banner.js
@@ -1,20 +1,55 @@
 var $cookieBanner = $('.global-cookie-message-dfe-sign-in');
 var $cookieAcceptButton = $cookieBanner.find('button.cookie-accept');
 
-var consentCookie = {};
-consentCookie.name = 'cookies_policy';
-consentCookie.payload = window.Cookies.get(consentCookie.name);
+var COOKIE_NAMES = {
+  PREFERENCES_SET: 'cookies_preferences_set',
+  POLICY: 'cookies_policy'
+};
 
-if (!consentCookie.payload) {
-    $cookieBanner.slideDown();
+var DEFAULT_POLICY = {
+  essential: true,
+  settings: false,
+  usage: false,
+  campaigns: false
+};
+
+var GOVUK_COOKIE_OPTIONS = {
+  expires: 365, // days
+};
+
+if (!window.Cookies.get(COOKIE_NAMES.PREFERENCES_SET)) {
+  window.Cookies.set(
+    COOKIE_NAMES.POLICY,
+    DEFAULT_POLICY,
+    GOVUK_COOKIE_OPTIONS
+  );
+
+  $cookieBanner.slideDown(function () {
+
+    $cookieAcceptButton.click(function () {
+      var acceptedPolicy = $.extend(DEFAULT_POLICY, {}); // deep copy
+      
+      window.Cookies.set(
+        COOKIE_NAMES.PREFERENCES_SET,
+        true,
+        GOVUK_COOKIE_OPTIONS
+      );
+
+      $.each(acceptedPolicy, function (key) {
+        acceptedPolicy[key] = true;
+      });
+
+
+      window.Cookies.set(
+        COOKIE_NAMES.POLICY,
+        acceptedPolicy,
+        GOVUK_COOKIE_OPTIONS
+      );
+
+      $cookieBanner.slideUp();
+      
+    });
+  
+  });
+  
 }
-// var consentCookie = {
-//     name: 'cookies_policy',
-//     payload: {
-//         essential: true,
-//         settings: false,
-//         usage: false,
-//         campaigns: false,
-//     }
-// };
-
